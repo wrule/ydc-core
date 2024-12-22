621f0a136a5be094c88c0f6c4969288c84f892b1
scott.gu <jimao@scottgudeMac-mini.local>
2024-12-22 18:19:24 +0800

    feat: ETH2YDC YDC2ETH

---
+++ contracts/YDC_AnchoredSwap.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import { Base } from "./base/Base.sol";
+
+contract YDC_AnchoredSwap is Base {
+  constructor() Base() { }
+
+  function ETH2YDC() public payable {
+
+  }
+
+  function YDC2ETH(uint256 ydcAmount) public {
+
+  }
+}
