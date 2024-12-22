1985b2525ca878c93b96fbe41a60b6361fcefad8
scott.gu <jimao@scottgudeMac-mini.local>
2024-12-22 11:34:16 +0800

    feat: Base.sol

---
+++ contracts/base/Base.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import { Ownable2Step, Ownable } from "@openzeppelin/contracts/access/Ownable2Step.sol";
+
+contract YDC_Base is Ownable2Step {
+  constructor() Ownable(_msgSender()) { }
+}
