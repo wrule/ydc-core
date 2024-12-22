98fc554122b0b4876989321ab6af9d12b7989443
scott.gu <jimao@scottgudeMac-mini.local>
2024-12-22 20:20:32 +0800

    feat: YDC_AnchoredSwap

--- contracts/YDC_AnchoredSwap.sol
+++ contracts/YDC_AnchoredSwap.sol
@@ -12,17 +12,33 @@ contract YDC_AnchoredSwap is Base, BaseUseRouter {
 
   YDC_Token ydcToken = YDC_Token(router.get("YDC_Token"));
 
+  error Error_InsufficientETH(address sender, uint256 ethAmount);
+  error Error_InsufficientYDC(address sender, uint256 ydcAmount);
+
+  event Event_ETH2YDC(address indexed sender, uint256 ethAmount, uint256 ydcAmount);
+  event Event_YDC2ETH(address indexed sender, uint256 ydcAmount, uint256 ethAmount);
+
   function ETH2YDC() public payable {
-    uint256 shares = msg.value / EQ_1YDC_ETH_AMOUNT;
-    ydcToken.mintFor(_msgSender(), shares);
-    uint256 change = msg.value - shares * EQ_1YDC_ETH_AMOUNT;
+    uint256 ydcAmount = msg.value / EQ_1YDC_ETH_AMOUNT;
+    if (ydcAmount < 1) {
+      revert Error_InsufficientETH(_msgSender(), msg.value);
+    }
+    uint256 ethAmount = ydcAmount * EQ_1YDC_ETH_AMOUNT;
+    ydcToken.mintFor(_msgSender(), ydcAmount);
+    uint256 change = msg.value - ethAmount;
     if (change > 0) {
       payable(_msgSender()).transfer(change);
     }
+    emit Event_ETH2YDC(_msgSender(), ethAmount, ydcAmount);
   }
 
   function YDC2ETH(uint256 ydcAmount) public {
+    if (ydcAmount < 1) {
+      revert Error_InsufficientYDC(_msgSender(), ydcAmount);
+    }
     ydcToken.transferFrom(_msgSender(), address(this), ydcAmount);
-    payable(_msgSender()).transfer(ydcAmount * EQ_1YDC_ETH_AMOUNT);
+    uint256 ethAmount = ydcAmount * EQ_1YDC_ETH_AMOUNT;
+    payable(_msgSender()).transfer(ethAmount);
+    emit Event_YDC2ETH(_msgSender(), ydcAmount, ethAmount);
   }
 }
