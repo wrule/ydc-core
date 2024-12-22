0e0c95ce5b58edcae0b63927c3bb6f9fdb22ea22
scott.gu <jimao@scottgudeMac-mini.local>
2024-12-22 18:00:42 +0800

    feat: YDC_Token

---
+++ contracts/YDC_Token.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import { ERC20Votes, ERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import { Base } from "./base/Base.sol";
+
+contract YDC_Token is ERC20, Base {
+  constructor() Base() ERC20("YiDeng College Token", "YD") { }
+
+  function mintFor(address user, uint256 amount) public onlyOwner {
+    _mint(user, amount);
+  }
+
+  function decimals() public view virtual override returns (uint8) {
+    return 0;
+  }
+}
