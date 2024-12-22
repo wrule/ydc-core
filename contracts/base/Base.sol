f5644270937b7cbfc5437f5f0b2e3f948079541f
scott.gu <jimao@scottgudeMac-mini.local>
2024-12-22 11:41:39 +0800

    feat: BaseERC721

--- contracts/base/Base.sol
+++ contracts/base/Base.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.28;
 
 import { Ownable2Step, Ownable } from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract YDC_Base is Ownable2Step {
+contract Base is Ownable2Step {
   constructor() Ownable(_msgSender()) { }
 }
---
+++ contracts/base/BaseERC721.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+import { ERC721URIStorage, ERC721 } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import { Base, Ownable } from "./Base.sol";
+
+contract BaseERC721 is ERC721URIStorage, Base {
+  using Strings for uint64;
+
+  uint256 private _nextTokenId;
+  string private baseURI;
+
+  constructor() Base() ERC721("YiDeng College Course", "YDCCourse") { }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
+
+  function setBaseURI(string memory newBaseURI) public onlyOwner {
+    baseURI = newBaseURI;
+  }
+
+  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    _requireOwned(tokenId);
+    return bytes(baseURI).length > 0 ? string.concat(
+      baseURI,
+      "?id=1"
+    ) : "";
+  }
+}
