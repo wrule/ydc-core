9f100e708a92be316b53dccdbddc9cd52e6ecbcd
jimao <shihao.gu@perfma.com>
2024-12-23 20:52:12 +0800

    feat: copy

--- contracts/Test_Chainlink.sol
+++ contracts/Test_Chainlink.sol
@@ -1,32 +1,73 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
-import "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import { Chainlink, ChainlinkClient } from "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import { ConfirmedOwner } from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import { LinkTokenInterface } from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
 
-contract Test_Chainlink is ChainlinkClient, ConfirmedOwner {
+contract APIConsumer is ChainlinkClient, ConfirmedOwner {
   using Chainlink for Chainlink.Request;
 
-  uint256 public num;
-
+  uint256 public volume;
   bytes32 private jobId;
   uint256 private fee;
+
+  event RequestVolume(bytes32 indexed requestId, uint256 volume);
 
   constructor() ConfirmedOwner(msg.sender) {
     _setChainlinkToken(0x779877A7B0D9E8603169DdbD7836e478b4624789);
     _setChainlinkOracle(0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD);
-    jobId = "7d80a6386ef543a3abb52817f6707e3b";  // HTTP Get > uint256 job
-    fee = (1 * LINK_DIVISIBILITY) / 10; // 0.1 LINK
+    jobId = "ca98366cc7314957b8c012c72f05aeeb";
+    fee = (1 * LINK_DIVISIBILITY) / 10;
   }
 
-  function updateNum() public returns (bytes32 requestId) {
-    Chainlink.Request memory req = _buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-    req._add("get", "https://pub-957003e8a7b049aaa00dfe01e18fd1e0.r2.dev/link.json");
-    req._add("path", "num");
+  function requestVolumeData() public returns (bytes32 requestId) {
+    Chainlink.Request memory req = _buildChainlinkRequest(
+        jobId,
+        address(this),
+        this.fulfill.selector
+    );
+
+    // Set the URL to perform the GET request on
+    req._add(
+        "get",
+        "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD"
+    );
+
+    // Set the path to find the desired data in the API response, where the response format is:
+    // {"RAW":
+    //   {"ETH":
+    //    {"USD":
+    //     {
+    //      "VOLUME24HOUR": xxx.xxx,
+    //     }
+    //    }
+    //   }
+    //  }
+    // request.add("path", "RAW.ETH.USD.VOLUME24HOUR"); // Chainlink nodes prior to 1.0.0 support this format
+    req._add("path", "RAW,ETH,USD,VOLUME24HOUR"); // Chainlink nodes 1.0.0 and later support this format
+
+    // Multiply the result by 1000000000000000000 to remove decimals
+    int256 timesAmount = 10 ** 18;
+    req._addInt("times", timesAmount);
+
+    // Sends the request
     return _sendChainlinkRequest(req, fee);
   }
 
-  function fulfill(bytes32 _requestId, uint256 _num) public recordChainlinkFulfillment(_requestId) {
-    num = _num;
+  function fulfill(
+    bytes32 _requestId,
+    uint256 _volume
+  ) public recordChainlinkFulfillment(_requestId) {
+    emit RequestVolume(_requestId, _volume);
+    volume = _volume;
+  }
+
+  function withdrawLink() public onlyOwner {
+    LinkTokenInterface link = LinkTokenInterface(_chainlinkTokenAddress());
+    require(
+      link.transfer(msg.sender, link.balanceOf(address(this))),
+      "Unable to transfer"
+    );
   }
 }
