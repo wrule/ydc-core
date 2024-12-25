a2864d5dda7ea7e5711302896d9dbdcccee69376
jimao <shihao.gu@perfma.com>
2024-12-25 12:17:46 +0800

    feat: YDC_DAO_Proposal

--- contracts/YDC_DAO.sol
+++ contracts/YDC_DAO.sol
@@ -4,6 +4,18 @@ pragma solidity ^0.8.28;
 import { BaseUseRouter } from "./base/BaseUseRouter.sol";
 import { Ownable2Step } from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
+struct YDC_DAO_Proposal {
+  uint64 id;
+  address target;
+  bytes action;
+  string prototype;
+  string summary;
+  uint256 yesVotes;
+  uint256 noVotes;
+  uint256 deadline;
+  uint64 prevId;
+}
+
 contract YDC_DAO is BaseUseRouter {
   constructor() BaseUseRouter() { }
 
