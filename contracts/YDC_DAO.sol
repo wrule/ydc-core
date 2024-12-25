992f4fcb0e19c1ef9c9049f6c2152ba55ffbf9ea
jimao <shihao.gu@perfma.com>
2024-12-25 12:38:54 +0800

    feat: initiateProposal

--- contracts/YDC_DAO.sol
+++ contracts/YDC_DAO.sol
@@ -12,12 +12,39 @@ struct YDC_DAO_Proposal {
   string summary;
   uint256 yesVotes;
   uint256 noVotes;
-  uint256 deadline;
+  uint256 createdAt;
+  uint256 deadlineAt;
   uint64 prevId;
 }
 
 contract YDC_DAO is BaseUseRouter {
   constructor() BaseUseRouter() { }
+
+  uint64 currentProposalId = 0;
+  mapping(uint64 => YDC_DAO_Proposal) mapProposal;
+
+  function initiateProposal(
+    address target,
+    bytes memory action,
+    string memory prototype,
+    string memory summary,
+    uint256 deadlineAt
+  ) public returns (uint64) {
+    currentProposalId++;
+    mapProposal[currentProposalId] = YDC_DAO_Proposal({
+      id: currentProposalId,
+      target: target,
+      action: action,
+      prototype: prototype,
+      summary: summary,
+      yesVotes: 0,
+      noVotes: 0,
+      createdAt: block.timestamp,
+      deadlineAt: deadlineAt,
+      prevId: currentProposalId - 1
+    });
+    return currentProposalId;
+  }
 
   function acceptOwnershipForMe(address target) public {
     Ownable2Step(target).acceptOwnership();
